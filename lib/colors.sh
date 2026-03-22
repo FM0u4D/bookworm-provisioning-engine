ccf28c3b6e4bb46258f433fd12504d7342b001fb
FM0u4D <fazzarimouaad@gmail.com>
2026-03-22 11:52:18 +0100

    Initial commit

--- lib/colors.sh
+++ lib/colors.sh
@@ -34,7 +34,7 @@ GREEN=$(_color "\033[38;5;82m")
 YELLOW=$(_color "\033[38;5;220m")
 ORANGE=$(_color "\033[1;33m")
 BLUE=$(_color "\033[38;5;39m")
-MAGENTA=$(_color "\033[38;5;151m")
+MAGENTA=$(_color "\033[38;5;131m")
 CYAN=$(_color "\033[38;5;51m")
 
 # Semantic aliases (VERY IMPORTANT for maintainability)
