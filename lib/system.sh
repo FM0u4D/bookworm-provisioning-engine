af2cfe728a0cebdc940807a0e3505c123cc90591
FM0u4D <fazzarimouaad@gmail.com>
2026-03-29 08:38:08 +0200

    Rectifying the is_service_active helper

--- lib/system.sh
+++ lib/system.sh
@@ -64,7 +64,11 @@ start_service() {
 }
 
 is_service_active() {
-    systemctl is-active --quiet "$1"
+    if [[ ! systemctl is-active --quiet "$1" ]]; then
+        log_fail "$1 service is not running"
+    else
+        log_success "$1 service is running perfectly"
+    fi
 }
 
 # ============================================
